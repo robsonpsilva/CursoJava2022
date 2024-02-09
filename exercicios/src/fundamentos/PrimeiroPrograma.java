eae15f539533dd745c39ac7d9ba9ae4cadb9ec57
robsonpsilva <robson_p_s@yahoo.com.br>
2024-02-09 17:44:29 -0300

    modified:   exercicios/src/fundamentos/PrimeiroPrograma.java

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -4,5 +4,6 @@ public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 		System.out.println("Bem-vindo ao primeiro programa!!!");
+		System.out.println("Testando git no eclipse!");
 	}
 }
