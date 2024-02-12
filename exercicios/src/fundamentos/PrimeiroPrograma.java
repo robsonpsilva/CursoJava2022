caccba6e75be8fb977b8935f0df0d137c1ff34fc
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-02-12 00:16:16 -0300

    TESTE DE COMMIT

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -5,6 +5,6 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		System.out.println("Bem-vindo ao primeiro programa!!!");
 		System.out.println("Testando git no eclipse!");
-		System.out.println("terceiro teste");
+		System.out.println("mais um teste");
 	}
 }
