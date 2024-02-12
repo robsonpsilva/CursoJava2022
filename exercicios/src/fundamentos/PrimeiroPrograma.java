ea7c733439322eda606ba45714c4192587a899d0
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-02-11 23:35:34 -0300

    TEWSTE
    TEST4E

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -5,5 +5,6 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		System.out.println("Bem-vindo ao primeiro programa!!!");
 		System.out.println("Testando git no eclipse!");
+		
 	}
 }
