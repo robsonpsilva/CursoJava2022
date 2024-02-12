4af70617db6d915287a7db5e18365ea362288cc9
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-02-12 00:01:34 -0300

    Add files via upload

---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		System.out.println("Bem-vindo ao primeiro programa!!!");
+		System.out.println("Testando git no eclipse!");
+		System.out.println("terceiro teste");
+	}
+}
