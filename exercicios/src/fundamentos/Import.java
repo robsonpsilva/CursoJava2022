4b16d481627a3d90da9cd5fb887b49b9e39bd46c
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-02-17 16:21:54 -0300

    Update Import.java
    import

--- exercicios/src/fundamentos/Import.java
+++ exercicios/src/fundamentos/Import.java
@@ -1,10 +1,17 @@
 package fundamentos;
 
+import java.util.Date;
+
+import javax.swing.JButton;
+
 public class Import {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Date d = new Date();
+		System.out.println(d);
+		JButton botao = new JButton(); 
 	}
 
 }
