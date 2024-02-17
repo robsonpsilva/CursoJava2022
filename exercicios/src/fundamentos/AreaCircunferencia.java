ccf0ed1b9a2692689480b5a3d9447f76aa9fbd77
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-02-17 10:35:05 -0300

    Update AreaCircunferencia.java
    Aula a respeito de constantes em Java

--- exercicios/src/fundamentos/AreaCircunferencia.java
+++ exercicios/src/fundamentos/AreaCircunferencia.java
@@ -4,9 +4,12 @@ public class AreaCircunferencia {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int raio = 3;
+		double raio = 3.4;
+		final double PI = 3.14159; //The keyword final converts a variable in a constant
+									//It is a convention to use constant names using CAPS letters
+		double area = PI*Math.pow(raio, 2);
 		
-		System.out.println();
+		System.out.println(area);
 
 	}
 
